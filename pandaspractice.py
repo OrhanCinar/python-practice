b295e6201d632bdb1932445374251a013b3f74bf
Orhan CINAR <orhancinar35@gmail.com>
2019-08-22 01:01:31 +0200

    add rolling mean

--- pandaspractice.py
+++ pandaspractice.py
@@ -9,21 +9,27 @@ df1 = pd.DataFrame(index=dates)
 
 
 for symbol in symbols:
-    df_tmp = pd.read_csv("data\{}.csv".format(symbol), index_col='Date', parse_dates=True, usecols=[
+    df_tmp = pd.read_csv("data\\{}.csv".format(symbol), index_col='Date', parse_dates=True, usecols=[
         'Date', 'Adj Close'], na_values=['nan'])
     df_tmp = df_tmp.rename(columns={'Adj Close': symbol})
     df1 = df1.join(df_tmp)
 
 
 df1 = df1.dropna()
-print(df1.ix['2010-01-01':'2010-01-10'], ['GOOG'])
 
+ax = df1['SPY'].plot(title='Stock rolling mean', label='SPY')
 
-ax = df1.plot(title='Stock prices')
 ax.set_xlabel('Date')
 ax.set_ylabel('Price')
+ax.legend(loc='upper left')
+
+rm_SPY = df1['SPY'].rolling(window=20).mean().plot(label='Rolling mean', ax=ax)
+
+df1['SPY'].plot()
 plt.show()
 '''
+
+
 
 
 
