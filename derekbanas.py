f227f69bcf4be8c53c582401445d05f9b023a3fd
Orhan CINAR <orhancinar35@gmail.com>
2019-08-28 15:38:49 +0200

    more practice

---
+++ derekbanas.py
@@ -0,0 +1,38 @@
+import sys
+import math
+import threading
+import time
+from functools import reduce
+
+
+'''
+name = input("What is your name ")
+print("Hi", name)
+cn1 = 5 + 6j
+
+print(cn1.real)
+print(cn1.imag)
+print(cn1)
+print("Cast", type(int(5.4)))
+print("Cast", type(str(5.4)))
+print(12, 21, 1971, sep='/')
+print(12, 21, 1971, end='')
+print("\n%04d %s %.2f %c" % (1, "Robot", 1.234, 'A'))
+print("5 + 2 =", 5 + 2)
+print(math.inf > 0)
+print(math.inf - math.inf)
+
+age = 30
+canVote = True if age >= 18 else False
+print(canVote)
+
+one_to_4 = range(1, 5)
+
+
+def times2(x): return x*2
+
+
+print(list(map(times2, one_to_4)))
+print(list(filter((lambda x: x % 2 == 0), range(1, 11))))
+print(reduce((lambda x, y: x+y), range(1, 6)))
+'''
